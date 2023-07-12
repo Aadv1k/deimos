dc73c7339ff5e1730cdd926b5dff21d77bfa687c
aadv1k <aadv1k@outlook.com>
2023-07-12 22:31:53 +0530

    chore: add `--help` to the CLI interface

--- image.c
+++ image.c
@@ -5,21 +5,21 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "lib/stb_image_write.h"
 
-void cv_load_image(Image* image) {
-  image->bytes = (unsigned char *)stbi_load(image->path, &(image->width), &(image->height), &(image->channels), 0);
-  if(image->bytes == NULL) {
+void cv_load_image(Image *image) {
+  image->bytes = (unsigned char *)stbi_load(
+      image->path, &(image->width), &(image->height), &(image->channels), 0);
+  if (image->bytes == NULL) {
     printf("Error in loading the image\n");
     exit(1);
   }
 }
 
-void cv_write_image(Image* image, const char * name) {
-  if (stbi_write_png(name, image->width, image->height, image->channels, image->bytes, 0) == 0) {
+void cv_write_image(Image *image, const char *name) {
+  if (stbi_write_png(name, image->width, image->height, image->channels,
+                     image->bytes, 0) == 0) {
     printf("Error writing PNG file.\n");
     exit(1);
   }
 }
 
-void cv_free_image(Image * image) {
-  free(image->bytes);
-}
+void cv_free_image(Image *image) { free(image->bytes); }
--- main.c
+++ main.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "./image.h"
 
 #include "./filters/blur.h"
 #include "./filters/grayscale.h"
-#include "./filters/sharpen.h"
 #include "./filters/median.h"
+#include "./filters/sharpen.h"
 
-void usage(const char * caller) {
+void usage(const char *caller) {
   printf("Usage:\n");
   printf("\t%s <args> input output\n", caller);
   printf("Examples:\n");
   printf("\t%s --blur --sigma 3.4 input.png output.png\n", caller);
   printf("\t%s --gray input.png output.png\n", caller);
   printf("Options:\n");
+  printf("\t--help        print this help message\n");
   printf("\t--blur        apply gaussian blur to the image\n");
   printf("\t--median      apply median filter to the image\n");
   printf("\t--gray        convert image to grayscale\n");
@@ -24,8 +25,8 @@ void usage(const char * caller) {
   printf("\t--radius      specify the radius for the blur (default 1.0)\n");
 }
 
-int main(int argc, char ** argv) {
-    const char * caller = argv[0];
+int main(int argc, char **argv) {
+  const char *caller = argv[0];
 
   if (argc == 1) {
     usage(caller);
@@ -37,44 +38,47 @@ int main(int argc, char ** argv) {
   int enableSharpen = 0;
   int enableMedian = 0;
   int cmdc = 0;
-  float blurSigma  = 1.0;
+  float blurSigma = 1.0;
   float sharpStrength = 0.5;
 
-    for (int i = 1; i < argc; i++) {
-        const char* current = argv[i];
+  for (int i = 1; i < argc; i++) {
+    const char *current = argv[i];
 
-        if (strcmp(current, "--blur") == 0) {
-            enableBlur = 1;
-            cmdc++;
-        } else if (strcmp(current, "--median") == 0) {
-            enableMedian = 1;
-            cmdc++;
-        } else if (strcmp(current, "--sharpen") == 0) {
-            enableSharpen = 1;
-            cmdc++;
-        } else if (strcmp(current, "--gray") == 0) {
-            enableGrayscale = 1;
-            cmdc++;
-          } else if (strcmp(current, "--radius") == 0) {
-            cmdc+=2;
+    if (strcmp(current, "--blur") == 0) {
+      enableBlur = 1;
+      cmdc++;
+    } else if (strcmp(current, "--help") == 0) {
+        usage(caller);
+        exit(0);
+    } else if (strcmp(current, "--median") == 0) {
+      enableMedian = 1;
+      cmdc++;
+    } else if (strcmp(current, "--sharpen") == 0) {
+      enableSharpen = 1;
+      cmdc++;
+    } else if (strcmp(current, "--gray") == 0) {
+      enableGrayscale = 1;
+      cmdc++;
+    } else if (strcmp(current, "--radius") == 0) {
+      cmdc += 2;
 
-            if (i + 1 < argc) {
-                blurSigma = atof(argv[i + 1]);
-                i++;
-            } else {
-                fprintf(stderr, "Error: --radius requires a value.\n");
-                exit(1);
-            }
-        } else if (strcmp(current, "--strength") == 0) {
-            cmdc+=2;
+      if (i + 1 < argc) {
+        blurSigma = atof(argv[i + 1]);
+        i++;
+      } else {
+        fprintf(stderr, "Error: --radius requires a value.\n");
+        exit(1);
+      }
+    } else if (strcmp(current, "--strength") == 0) {
+      cmdc += 2;
 
-            if (i + 1 < argc) {
-                sharpStrength = atof(argv[i + 1]);
-                i++;
-            } else {
-                fprintf(stderr, "Error: --strength requires a value.\n");
-                exit(1);
-            }
+      if (i + 1 < argc) {
+        sharpStrength = atof(argv[i + 1]);
+        i++;
+      } else {
+        fprintf(stderr, "Error: --strength requires a value.\n");
+        exit(1);
+      }
     }
   }
 
@@ -83,34 +87,31 @@ int main(int argc, char ** argv) {
     exit(1);
   }
 
-  const char* input_path = argv[argc - 2];
-  const char* output_path = argv[argc - 1];
+  const char *input_path = argv[argc - 2];
+  const char *output_path = argv[argc - 1];
 
-  Image img = { .path = input_path };
+  Image img = {.path = input_path};
   cv_load_image(&img);
 
-  if (enableGrayscale)  {
+  if (enableGrayscale) {
     cv_apply_grayscale(&img);
     printf("Info: applied grayscale\n");
   }
 
-
-  if (enableMedian)  {
+  if (enableMedian) {
     cv_apply_median_filter(&img, 5);
     printf("Info: applied median filter\n");
   }
 
-  if (enableSharpen)  {
+  if (enableSharpen) {
     cv_apply_sharpening(&img, sharpStrength);
     printf("Info: applied sharpen\n");
   }
 
-
-  if (enableBlur)  {
+  if (enableBlur) {
     cv_apply_gaussian_blur(&img, blurSigma, 9);
     printf("Info: applied gaussian blur of intensity %.2f\n", blurSigma);
   }
-
 
   cv_write_image(&img, output_path);
 
