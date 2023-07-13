e8e04b3216ed2a91068ab2e112075864f60d8cfe
aadv1k <aadv1k@outlook.com>
2023-07-13 22:52:08 +0530

    chore: add build.sh

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Create the bin directory if it doesn't exist
+if [ ! -d "bin" ]; then
+        mkdir bin
+        fi
+
+# Set the compiler and flags
+CC=gcc
+CFLAGS="-Wall -Werror"
+
+# Set the source files
+SRCS="main.c image.c filters/blur.c filters/grayscale.c filters/sharpen.c filters/median.c filters/bilateral.c"
+
+# Compile the source files
+$CC $CFLAGS $SRCS -o bin/cv
+
