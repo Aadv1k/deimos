9da41b019f8079a648b56e22ddb63d85db30502a
aadv1k <aadv1k@outlook.com>
2023-07-26 11:05:49 +0530

    feat: implement 180deg rotation

--- src/transformations/rotate.c
+++ src/transformations/rotate.c
@@ -1,14 +1,32 @@
 #include "../include/transformations/rotate.h"
-
 #include <assert.h>
 
-void deimos_apply_rotation(Image * img, RotationDegree deg) {
-  switch (deg) {
+void rotationSwap(unsigned char* x, unsigned char* y) {
+    unsigned char temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void deimos_apply_rotation(Image* img, RotationDegree deg) {
+
+    int width = img->width,
+        height = img->height,
+        ch = img->channels;
+
+    switch (deg) {
     case (DEG_180):
-      (void)img;
-      assert(0 && "Not implemente");
-      break;
+        for (int i = 0; i < height / 2; i++) {
+            for (int j = 0; j < width; j++) {
+                for (int c = 0; c < ch; c++) {
+                  rotationSwap(
+                               &img->bytes[(i * width + j) * ch + c],
+                               &img->bytes[((height - 1 - i) * width + (width - 1 - j)) * ch + c]
+                    );
+                }
+            }
+        }
+        break;
     default:
-      assert(0 && "Not a valid degree");
-  }
+        assert(0 && "Not a valid degree");
+    }
 }
