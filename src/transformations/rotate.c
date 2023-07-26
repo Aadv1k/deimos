cefdc720b8c4b5fd050ac451c5692c8f418ac0d2
aadv1k <aadv1k@outlook.com>
2023-07-26 11:24:01 +0530

    feat: implement the 90deg rotation

--- src/transformations/rotate.c
+++ src/transformations/rotate.c
@@ -1,5 +1,6 @@
 #include "../include/transformations/rotate.h"
 #include <assert.h>
+#include <stdlib.h>
 
 void rotationSwap(unsigned char* x, unsigned char* y) {
     unsigned char temp = *x;
@@ -26,6 +27,31 @@ void deimos_apply_rotation(Image* img, RotationDegree deg) {
             }
         }
         break;
+     case (DEG_90):
+        Image* rotated_img = (Image*)malloc(sizeof(Image));
+        rotated_img->width = height;
+        rotated_img->height = width;
+        rotated_img->channels = ch;
+        rotated_img->bytes = (unsigned char*)malloc(rotated_img->width * rotated_img->height * rotated_img->channels);
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                for (int c = 0; c < ch; c++) {
+                    rotated_img->bytes[(j * rotated_img->width + (height - 1 - i)) * ch + c] = img->bytes[(i * width + j) * ch + c];
+                }
+            }
+        }
+
+        free(img->bytes);
+
+        img->width = rotated_img->width;
+        img->height = rotated_img->height;
+        img->channels = rotated_img->channels;
+        img->bytes = rotated_img->bytes;
+
+        free(rotated_img);
+        break;
+       break;
     default:
         assert(0 && "Not a valid degree");
     }
